b1ef8ecd94aecddbf8c48fd127606b676bed57e9
Michael <m.m.downing@bigpond.com>
2024-03-16 10:32:38 +0800

    added dynamic NavBar title

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+
 export default function NavBar() {
+  const pathname = usePathname();
+  const [title, setTitle] = useState("STATE TEAM CHAMPIONSHIP LADDER");
+
+  useEffect(() => {
+    if (pathname == "/login") {
+      setTitle("Login");
+    }
+  }, [pathname]);
+
   return (
     <div className="flex bg-gradient-to-r from-yellow-200 to-yellow-500 h-24 w-full justify-between items-center px-4">
       <div>Logo</div>
-      <div className="text-2xl font-bold">STATE TEAM CHAMPIONSHIP LADDER</div>
+      <div className="text-2xl font-bold">{title}</div>
       <div>Search Bar</div>
     </div>
   );
