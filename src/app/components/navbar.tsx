efe0aabd4a5e8105724ac1cba02078e4e7a82c43
Michael <m.m.downing@bigpond.com>
2024-03-16 10:10:46 +0800

    initial commit also NavBar lmao

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <div className="flex bg-gradient-to-r from-yellow-200 to-yellow-500 h-24 w-full justify-between items-center px-4">
+      <div>Logo</div>
+      <div className="text-2xl font-bold">STATE TEAM CHAMPIONSHIP LADDER</div>
+      <div>Search Bar</div>
+    </div>
+  );
+}
